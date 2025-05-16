27d042b38d467153f10f1ec308ec1a7e1039f55f
Your Name <you@example.com>
2025-05-16 13:40:52 +0300

    Home page commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
-
+import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectTheme } from './redux/features/themeSlice';
-
+import Footer from './components/Footer/Footer';
 function App() {
   const theme = useSelector(selectTheme);
 
   return (
-      <div className={`${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'}`}>
-        <Navbar />
-        <Home />
-      </div>
+    <div className={`${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'}`}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './features/themeSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['theme']
+}
+
+const persistedReducer = persistReducer(persistConfig, themeReducer)
 
 export const store = configureStore({
   reducer: {
-    theme: themeReducer,
+    theme: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+      }
+    })
 })
+
+export const persistor = persistStore(store)
 
 export default store 
