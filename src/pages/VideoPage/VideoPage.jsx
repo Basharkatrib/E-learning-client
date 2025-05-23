26b3c2fecf23dc1711582cb13207dddb0790c087
Mjd Hanna <mjdhna67@gmail.com>
2025-05-23 17:31:03 +0300

    view video

---
+++ src/pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ViewVideo from '../../components/ViewVideo/ViewVideo'
+
+const VideoPage = () => {
+    return (
+        <div>
+            <ViewVideo />
+        </div>
+    )
+}
+
+export default VideoPage
