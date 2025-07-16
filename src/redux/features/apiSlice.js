5d65b2e2f4214f1128b7d5b4858d2c3f9aeeb54a
Your Name <you@example.com>
2025-07-17 02:57:13 +0300

    Add note page2

--- src/redux/features/apiSlice.js
+++ src/redux/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://e-learning-server.test/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://e-learning-server-me-production.up.railway.app/api/' }),
   endpoints: (builder) => ({
     register: builder.mutation({
       query: (userData) => ({
